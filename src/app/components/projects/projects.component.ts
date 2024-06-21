2fc5230e1bf7ac3a00bf8e2777991483cc09e565
Mathieu Domichard <mathieu.domichard@gmail.com>
2024-06-21 14:50:58 +0200

    Project section start

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,22 +1,47 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  AfterViewInit,
+  HostListener,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css'],
 })
-export class ProjectsComponent {
-  projects = [
-    {
-      name: 'Project 1',
-      description: 'Description of project 1',
-      link: 'http://example.com/project1',
-    },
-    {
-      name: 'Project 2',
-      description: 'Description of project 2',
-      link: 'http://example.com/project2',
-    },
-    // Ajoutez d'autres projets ici
-  ];
+export class ProjectsComponent implements AfterViewInit {
+  @ViewChild('circle') circle!: ElementRef;
+  @ViewChild('content') content!: ElementRef;
+
+  ngAfterViewInit() {
+    this.adjustCircleSize();
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    this.adjustCircleSize();
+  }
+
+  adjustCircleSize() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const windowHeight = window.innerHeight;
+    const circleElement = this.circle.nativeElement as HTMLElement;
+    const circleTop = circleElement.getBoundingClientRect().top + scrollTop;
+
+    // Commence l'animation seulement quand le cercle est visible
+    if (scrollTop + windowHeight > circleTop) {
+      const startScroll = circleTop - windowHeight;
+      const midScrollTop = startScroll + windowHeight / 2;
+      const maxScrollTop = startScroll + windowHeight;
+      const scrollPercent =
+        (scrollTop - midScrollTop) / (maxScrollTop - midScrollTop);
+
+      const clampedScrollPercent = Math.min(Math.max(scrollPercent, 0), 1);
+      const newSize = 400 + windowHeight * clampedScrollPercent * 2;
+      circleElement.style.width = `${newSize}px`;
+      circleElement.style.height = `${newSize}px`;
+    }
+  }
 }
