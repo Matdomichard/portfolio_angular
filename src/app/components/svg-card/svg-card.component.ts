53d0305a7023e9505e71a04d94c1b6988ee7f873
Mathieu Domichard <mathieu.domichard@gmail.com>
2024-06-22 00:37:44 +0200

    Cards update

--- src/app/components/svg-card/svg-card.component.ts
+++ src/app/components/svg-card/svg-card.component.ts
@@ -6,10 +6,10 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./svg-card.component.css'],
 })
 export class SvgCardComponent {
-  @Input() title1: string = 'Backend';
+  @Input() title1: string = 'BACKEND';
   @Input() content1: string =
     'Experience in Spring Boot, Java, and microservices.';
-  @Input() title2: string = 'Frontend';
+  @Input() title2: string = 'FRONTEND';
   @Input() content2: string =
     'Proficient in Angular, TypeScript, and responsive design.';
 }
