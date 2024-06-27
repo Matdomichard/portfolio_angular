827a95b02f80bec85514aae1fc2e41fad721b417
Mathieu Domichard <mathieu.domichard@gmail.com>
2024-06-27 13:55:03 +0200

    Responsive work

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
-
 import 'zone.js/dist/zone';
-import { gsap } from 'gsap';
+
+function enforceMinWidth() {
+  if (window.innerWidth < 500) {
+    document.documentElement.style.minWidth = '450px';
+    document.body.style.minWidth = '450px';
+  } else {
+    document.documentElement.style.minWidth = 'auto';
+    document.body.style.minWidth = 'auto';
+  }
+}
+
+window.addEventListener('resize', enforceMinWidth);
+window.addEventListener('load', enforceMinWidth);
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
