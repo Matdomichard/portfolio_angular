a0e3cc2b2bec84d6eff8d5fa62e8700c89860526
mdomichard <mathieu.domichard@gmail.com>
2025-03-14 14:42:07 +0100

    Added emailing

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css'],
+  styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
   contact = {
     name: '',
     email: '',
-    message: '',
+    message: ''
   };
 
-  onSubmit() {
-    console.log('Contact Form Submitted', this.contact);
+  public sendEmail(e: Event) {
+    e.preventDefault();
+    emailjs.sendForm('service_xqcx6rd', 'template_10mv2lr', e.target as HTMLFormElement, '9YDpWTslw1tyVx4vs')
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
   }
 }
